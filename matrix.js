8d1df78b20870d1f335bfff9b0a84d5e8b9ca95f
Alok Chandra <aloknonden234@gmail.com>
2022-02-22 01:52:18 +0600

    first commit

---
+++ matrix.js
@@ -0,0 +1,89 @@
+ var num=3;
+ var str="";
+ for(let i=0; i<=num;i++){
+    //  console.log(i);
+
+     for(let j=0; j<i;j++){
+        
+        str +="*";
+        
+
+     }
+     str +="\n";
+     
+ }
+//  console.log(str);
+
+
+ var num=2;
+ var str="";
+ for(let i=num; i>=1;i--){
+    //  console.log(i);
+
+     for(let j=1; j<=i;j++){
+        
+        str +="*";
+        
+        
+
+     }
+     str +="\n";
+     
+ }
+//  console.log(str);
+
+
+var num=5;
+ var str="";
+ 
+
+ for (var i=1; i<=num; i++){
+    for(var j=1; j<=num-i; j++){
+       str +=" ";
+    }
+
+    for(var k=1;k<=i;k++){
+       str +="*"
+    }
+
+    str +="\n"
+ }
+//  console.log(str);
+
+
+
+//  var num=5;
+//  var str="";
+ 
+
+//  for (var i=num; i>=1; i--){
+//     for(var j=1; j<=num; j++){
+//        str +=" ";
+//     }
+//     for(var k=1;k<=i;k++){
+//        str +="*"
+//     }
+//     str +="\n"
+//  }                                     #### not understood.......
+//  console.log(str);
+
+
+
+
+
+// ################ matrix maltipication
+
+
+
+var matrix1=[
+   [10,20,30],
+   [40,50,60],
+   [70,80,90]
+]
+var matrix2=[
+   [1,2],
+   [4,5],
+   [7,8]
+]
+
+
